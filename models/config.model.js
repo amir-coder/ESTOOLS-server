3b0a1c1344cfee9ad3e9bf06001f05d5e325eaf4
amir-coder <ja_amir@esi.dz>
2022-11-07 12:36:19 -0800

    added: User and Config models

---
+++ models/config.model.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const Config = mongoose.model(
+  "Config",
+  new mongoose.Schema({
+    titre: String,
+    //TODO: create config realtions after creating the models
+  })
+);
