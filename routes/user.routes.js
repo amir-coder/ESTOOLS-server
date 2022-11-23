a9c8b2a1ab10806d126e99115f5d093aab948913
amir-coder <ja_amir@esi.dz>
2022-11-23 08:57:19 -0800

    added user route

---
+++ routes/user.routes.js
@@ -0,0 +1,21 @@
+const controller = require("../controllers/user.controller");
+const { authJwt } = require("../middleware");
+const express = require("express");
+
+const router = new express.Router();
+
+//TODO: implement router endpoints
+
+router.get("all/", controller.allAccess);
+router.get("user/", [authJwt.verifyToken], controller.userBoard);
+router.get(
+  "moderator/",
+  [authJwt.verifyToken, authJwt.isModerator],
+  controller.moderatorBoard
+);
+router.get(
+  "admin/",
+  [authJwt.verifyToken, authJwt.isAdmin],
+  controller.adminBoard
+);
+module.exports = router;
