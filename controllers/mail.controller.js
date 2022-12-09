f0683a454022874c70d286c1df3cedad79b6b865
amir-coder <ja_amir@esi.dz>
2022-12-09 10:41:47 -0800

    added sendMail function

--- controllers/mail.controller.js
+++ controllers/mail.controller.js
@@ -1,9 +1,31 @@
 const nodemailer = require("nodemailer");
+
+const google = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 const mailConfig = require("../config/email.config");
 const apiConfig = require("../config/api.config");
 
-module.exports.send_email = async (email, secretString) => {
-  var Transport = nodemailer.createTransport({
+const createTransporter = async () => {
+  const oauth2CLient = new OAuth2(
+    mailConfig.client_id,
+    mailConfig.client_secret,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2CLient.setCredentials({
+    refreshToken: mailConfig.refresh_token,
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2CLient.createAccessToken((err, token) => {
+      if (err) {
+        reject("Can't create access token :<!");
+      }
+      resolve(token);
+    });
+  });
+
+  var transporter = nodemailer.createTransport({
     service: "Gmail",
     host: "smtp.gmail.com",
     port: 465,
@@ -11,12 +33,23 @@ module.exports.send_email = async (email, secretString) => {
     auth: {
       type: "OAuth2",
       user: mailConfig.email,
+      accessToken,
       clientId: mailConfig.client_id,
       clientSecret: mailConfig.client_secret,
       refreshToken: mailConfig.refresh_token,
     },
   });
 
+  return transporter;
+};
+
+const sendMail = async (emailOptions) => {
+  transporter = await createTransporter();
+
+  transporter.sendEmail(emailOptions);
+};
+
+module.exports.send_email = async (email, secretString) => {
   let mailOptions = {
     from: mailConfig.email,
     to: email,
@@ -24,11 +57,5 @@ module.exports.send_email = async (email, secretString) => {
     html: `Press <a href="${apiConfig.HOST}:${apiConfig.PORT}/auth/verify/${email}/${secretString}" > Here <a/> To verify your email. Thanks.`,
   };
 
-  Transport.sendMail(mailOptions, (err, response) => {
-    if (err) {
-      console.log("ERROR:", err);
-    } else {
-      console.log("Message send");
-    }
-  });
+  sendMail(mailOptions);
 };
