b51763cdd12690d7d88cb010d30d2aafb586b442
meriemhmdn <115783617+meriemhmdn@users.noreply.github.com>
2022-12-11 14:35:58 +0100

    reminder model, add liste des dates delete the liste of config
    reminder model, add liste des dates delete the liste of config

--- models/reminders.model.js
+++ models/reminders.model.js
@@ -3,19 +3,18 @@ const mongoose = require("mongoose");
 const Reminders = mongoose.model(
   "Reminders",
   new mongoose.Schema({
-    dates: date,
+    dates: [Date],
     time: String,
     repeat : Number,
     daily: Boolean,
     weekly: Boolean,
     alarm: Boolean,
     notification: Boolean,
-    Config: [
+    Config: 
         {
           type: mongoose.Schema.Types.ObjectId,
           ref: "Config"
         }
-    ]
   })
 );
 
