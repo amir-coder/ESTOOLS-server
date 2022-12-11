87b93e1870984f246217aff89ecbe5fda8b1d0b4
meriemhmdn <115783617+meriemhmdn@users.noreply.github.com>
2022-12-11 20:43:07 +0100

    creating the notes model
    creating the notes model

---
+++ models/notes.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const notes = mongoose.model(
+  "notes",
+  new mongoose.Schema({
+    title: String,
+    description: String,
+    Category: 
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Categories_notes"
+      },
+    Config: 
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Categories_notes"
+      }
+  })
+);
+
+module.exports = notes;
