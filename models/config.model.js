407b1f7371c2aebc3976f020ab037cbe9402e7ef
meriemhmdn <115783617+meriemhmdn@users.noreply.github.com>
2022-12-11 09:03:52 +0100

    config model modification
    config model modification

--- models/config.model.js
+++ models/config.model.js
@@ -3,8 +3,14 @@ const mongoose = require("mongoose");
 const Config = mongoose.model(
   "Config",
   new mongoose.Schema({
-    titre: String,
-    //TODO: create config realtions after creating the models
+    title: String,
+    User: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user"
+      }
+    ]
+
   })
 );
 
