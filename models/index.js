575fe3a3a6b053e8fe0fd32d78cca6f3b2dc6c16
meriemhmdn <115783617+meriemhmdn@users.noreply.github.com>
2022-12-11 09:04:11 +0100

    index modification

--- models/index.js
+++ models/index.js
@@ -7,6 +7,12 @@ db.mongoose = mongoose;
 
 db.user = require("./user.model");
 db.role = require("./role.model");
+db.folder = require("./folder.model");
+db.reminders = require("./reminders.model");
+db.resources = require("./resources.model");
+db.tasks = require("./tasks.model");
+db.config = require("./config.model");
+db.categories = require("./categories.model");
 
 db.ROLES = ["student", "moderator", "admin"];
 
