caa34b071412eaade5c5c6bf58d4064d88cb97c4
meriemhmdn <115783617+meriemhmdn@users.noreply.github.com>
2022-12-11 20:36:35 +0100

    adding notes and categories model
    adding notes and categories model

--- models/index.js
+++ models/index.js
@@ -13,7 +13,8 @@ db.resources = require("./resources.model");
 db.tasks = require("./tasks.model");
 db.config = require("./config.model");
 db.categories_tasks = require("./categories_tasks.model");
-
+db.categories_notes = require("./categories_notes.model");
+db.notes = require("./notes.model");
 db.ROLES = ["student", "moderator", "admin"];
 
 module.exports = db;
